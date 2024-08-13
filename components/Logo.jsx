c1d6d54c17aed0ef446d07cbc0752eb4088810f5
gillpann <cpundong@gmail.com>
2024-08-13 15:21:00 +0700

    add logo header

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const Logo = () => {
     return (
         <Link href='/' className="flex items-center text-3xl font-bold hover:text-blue-700 transition duration-300 ease-in-out">
-            <Image src='/mylogo.png' alt="logo" width={40} height={40} className="mr-2"/>
+            <Image src='/logo.png' alt="logo" width={40} height={40} className="mr-2"/>
             <span className="gradient-text">Gilvan.</span>
         </Link>
     )
