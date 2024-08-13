320bea68192d8628c85ef31c130f88840614008a
gillpann <cpundong@gmail.com>
2024-08-13 14:55:17 +0700

    add logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Image from "next/image"
 
 const Logo = () => {
     return (
-        <Link href='/' className="text-blue-500 text-3xl font-bold hover:text-blue-700 transition duration-300 ease-in-out">
-            Gilvan.
+        <Link href='/' className="flex items-center text-3xl font-bold hover:text-blue-700 transition duration-300 ease-in-out">
+            <Image src='/mylogo.png' alt="logo" width={40} height={40} className="mr-2"/>
+            <span className="gradient-text">Gilvan.</span>
         </Link>
     )
 }
