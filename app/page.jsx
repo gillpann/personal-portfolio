37d16a75b91bd174cafe2efa7146d7d3c1418b27
gillpann <cpundong@gmail.com>
2025-01-19 19:55:55 +0700

    add welcome screen

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,24 @@
-
 // components
-import About from "@/components/About";
+import WelcomeScreen from "@/components/WelcomeScreen";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 import MarqueeDemo from "@/components/MarqueeDemo";
 import Services from "@/components/Services";
-import Work  from "@/components/Work";
+import Work from "@/components/Work";
 import Cta from "@/components/Cta";
-
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <About />
-      <MarqueeDemo />
-      <Services />
-      <Work />
-      <Cta />
-    </main>
+    <>
+      <WelcomeScreen />
+      <main>
+        <Hero />
+        <About />
+        <MarqueeDemo />
+        <Services />
+        <Work />
+        <Cta />
+      </main>
+    </>
   );
 }
---
+++ components/WelcomeScreen.jsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Socials from "@/components/Socials";
+
+const WelcomeScreen = () => {
+    const [isVisible, setIsVisible] = useState(true);
+    const [fadeOut, setFadeOut] = useState(false); 
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+        setFadeOut(true); 
+        }, 3500); 
+
+        const hideTimer = setTimeout(() => {
+        setIsVisible(false); 
+        }, 4500); 
+
+        return () => {
+        clearTimeout(timer);
+        clearTimeout(hideTimer);
+        };
+    }, []);
+
+    if (!isVisible) return null;
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-white dark:bg-black transition-all duration-1000">
+        <div className="absolute inset-0 bg-white dark:bg-black z-10"></div>{" "}
+        <div
+            className={`text-center z-20 transition-all duration-1000 ${
+            fadeOut ? "opacity-0" : "opacity-100"
+            }`}
+        >
+            {/* Logo Sosial */}
+            <div className="mb-4" data-aos="fade-down">
+            <Socials
+                containerStyles="flex justify-center gap-6"
+                iconsStyles="text-3xl text-primary hover:text-secondary transition-colors"
+            />
+            </div>
+
+            <div
+            className="text-sm uppercase font-semibold mb-4 tracking-[2px]"
+            data-aos="fade-down"
+            style={{ color: "var(--text-primary)" }}
+            >
+            Welcome to my
+            </div>
+
+            <h1
+            className="text-4xl uppercase md:text-6xl font-bold text-primary"
+            data-aos="fade-up"
+            >
+            Portfolio Website
+            </h1>
+        </div>
+        </div>
+    );
+};
+
+export default WelcomeScreen;
