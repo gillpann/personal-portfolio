b40560d287496eb7a0ad4af83b8009f4be47e85f
gillpann <cpundong@gmail.com>
2025-06-20 20:39:13 +0700

    update: head n logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -31,7 +31,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <title>Portfolio - Gilvan</title>
+        <title>Ahmad Gilvan | Web Developer Portfolio</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <meta name="google-site-verification" content="vU7EaUtw72ofuO2O9loJkXFTPcVOuxKS4BQSxCVUYMw" />
         <meta name="description" content="Halo, saya Gilvan! Ini adalah portofolio saya yang menampilkan proyek-proyek web development dengan sentuhan desain dan fungsionalitas." />
         <meta name="keywords" content="Ahmad Gilvan, Portfolio, Mahasiswa Informatika, Next.js, Tailwind, Web Developer" />
@@ -40,6 +41,9 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content="Lihat karya-karya Gilvan dalam bidang web development." />
         <meta property="og:url" content="https://ahmad-gilvan.vercel.app" />
         <meta name="robots" content="index, follow" />
+        <meta property="og:site_name" content="Gilvan Portfolio" />
+        <meta property="og:image" content="https://ahmad-gilvan.vercel.app/og-image.jpg" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={outfit.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
